e39dd6c5beb39a43e233947e657d961990fa261c
Tim <timowland@gmail.com>
2014-11-23 17:16:12 -0800

    more changes

--- js/signup.js
+++ js/signup.js
@@ -3,6 +3,7 @@
     This script will load the state select list and validate the form before submission
 */
 
+"use strict";
 
 /*creates the states drop down list */
 function onReady() {
@@ -19,38 +20,55 @@ function onReady() {
 		stateSelect.appendChild(option);
 	}
 
-	/* need to figure out this occupation crap */
 	/*When other is selected for occupation, creates field to enter in occupation manually*/
 	document.getElementById('occupation').addEventListener('change', function(){
-<<<<<<< HEAD
-		if(document.getElementById('occupation').value = "other") {
-			document.getElementByName('occupationOther').style.display = 'block'; 		
-=======
-		if(document.getElementById('occupation').value = 'other') {
-			document.getElementById('occupation').style.display = 'block'; 		
->>>>>>> b6c86e93c92686c69e1bb4addaff00663f199910
+		var occupationField = signupForm.elements['occupation'];
+		var actualOccupation = occupationField.value;
+		if(actualOccupation == 'other') {
+			document.getElementById('occupationOther').style.display = 'block'; 		
+		}else {
+			document.getElementById('occupationOther').style.display = 'none';
 		}
 	});
 
+	/*if no thanks is clicked then links to google.com if you hit ok*/
 	document.getElementById('cancelButton').addEventListener('click', function(){
 		var leavePage = confirm("Do you really want to leave the page?");
 		if(leavePage) {
 			location.href = "http://google.com";
 		}
 	});
-	signupForm.addEventListener('submit', onSubmit);
+	signupForm.addEventListener('submit', onSubmit); 
 }
 
+/* onSubmit()
+ * Called when the user attempts to submit the form
+ * The browser will pass an event object as the first parameter and we can use this object
+ * to stop the form from being submitted if it is invalid.
+ * Also the keyword 'this' will refer to the form that is being submitted while inside this function.
+ * */
 function onSubmit(evt) {
 	var valid = validateForm(this);
+    if (!valid) {
+        var errMsg = document.getElementById('error-message');
+        errMsg.innerHTML = 'Please provide values for the required fields!';
+        errMsg.style.display = 'block';
+    }
 
-	if(!valid) {
-		var 
-	}
+    if (!valid && evt.preventDefault) {
+        evt.preventDefault();
+    }
+    evt.returnValue = valid;
+    return valid;
+
 }
 
+
+
+/*tests the fields to make sure that there are actual inputted values
+*returns whether the values are valid*/
 function validateForm(form) {
-	var requiredFields =['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+	var requiredFields =['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate', 'occupation'];
 	var i;
 	var valid = true;
 	for(i = 0; i < requiredFields.length; i++) {
@@ -59,33 +77,54 @@ function validateForm(form) {
 		}
 	}
 }
-
+/*tests that the fields aren't just white space
+also tests zip code if zip code is accurate 
+checks birthdate to see if older than 13 years old
+if a field is not correct it shows that it is invalid*/
 function validateRequiredField(field) {
+	/*checks if user picked an occupation and also
+	checks if user picks other for occupation (if other: changes field value to the input box*/
+	if(field = 'occupation' && occupation.value = 'other') {
+		field = occupationOther;
+	}else if(field = 'occupation') {
+		field.className = 'form-control';
+		return valid;
+	}
+	/*tests for only blank space*/
 	var value = field.value;
 	value = value.trim();
 	var valid = value.length > 0;
+
+	/*tests for a 5 digit zip code*/
 	if(field = 'zip') {
 		var zipRegExp = new RegExp('^\\d{5}$');
-		if(!zipRegExp.test(form.elements[requiredFields[i]].value)) {
+		if(!zipRegExp.test(value)) {
 			valid = false;
 		}
-	}
+	} 
+
+	/*tests the birthdate to make sure it is >13 years old*/
 	if(field = 'birthdate') {
 		var t = new Date();
 		var today = t.getUTCDate();
 		var m = new Date();
 		var month = m.getUTCMonth();
 		var y = new Date();
-		var year = y.getUTCFullYear();
+		var year = y.getUTCFullYear(); 
 		if((year - birthdate.getUTCFullYear()) > 13) {  
 			valid = true;
-		}else if(year = birthdate.getUTCFullYear() && birthdate.getUTCMonth() > month) {
+		}else if(year == birthdate.value.getUTCFullYear() && birthdate.value.getUTCMonth() > month) {
 			valid = true;
-		}else if(year = birthdate.getUTCFullYear() && birthdate.getUTCMonth() = month && birthdate.getUTCDate() >= today) {
+		}else if(year == birthdate.value.getUTCFullYear() && birthdate.value.getUTCMonth() == month && birthdate.value.getUTCDate() >= today) {
 			valid = true;
 		}else {
 			valid = false;
-		}			
+		} 
+		if(valid = false) {
+			var errMsg = document.getElementById('birthdateMessage');
+			errMsg.innerHTML = 'Users must be at least 13 years old.'	
+			errMsg.style.display = 'block';
+		}	
 	}
 	if(valid) {
 		field.className = 'form-control';
